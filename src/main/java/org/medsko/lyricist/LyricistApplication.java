855c8bc28d50259efdb59d9e5261bf0b3ba523eb
Melle de Vries <melle@openweb.nl>
2020-05-03 11:16:01 +0200

    First commit of generated Spring boot app

---
+++ src/main/java/org/medsko/lyricist/LyricistApplication.java
@@ -0,0 +1,13 @@
+package org.medsko.lyricist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyricistApplication.class, args);
+    }
+
+}
